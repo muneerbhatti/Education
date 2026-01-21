262889bb5e3ab16d7487bf98100a009b0015a304
Rao-Ahmar <raoahmar87@gmail.com>
2026-01-21 12:52:09 +0500

    push code

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+//= require jquery-3.6.3.min
+//= require bootstrap.bundle.min
+//= require jquery.magnific-popup.min
+//= require jquery-validator
+//= require slick.min
+//= require wow
+//= require_tree .
