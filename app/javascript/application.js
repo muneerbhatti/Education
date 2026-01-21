24f5464a572c52e4fa16027d88691c662bbdeab5
amir <amirad2020@gmail.com>
2026-01-21 17:54:57 +0500

    js load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery-3.6.3.min
-//= require bootstrap.bundle.min
-//= require jquery.magnific-popup.min
-//= require jquery-validator
-//= require slick.min
-//= require wow
-//= require_tree .
+import "bootstrap"
+import "jquery1"
+import "jquery-appear"
+import "jquery-validator"
+import "jquery-countdown"
+import "jquery-magnific-popup"
+import "slick"
+import "jquery2"
+import "WOW"
+import "app"
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,14 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin 'bootstrap', to: 'bootstrap.bundle.min.js'
+pin 'jquery1', to: 'jquery-3.6.3.min.js'
+pin 'jquery-appear', to: 'jquery-appear.js'
+pin 'jquery-validator', to: 'jquery-validator.js'
+pin 'jquery-countdown', to: 'jquery.countdown.min.js'
+pin 'jquery-magnific-popup', to: 'jquery.magnific-popup.min.js'
+pin 'slick', to: 'slick.min.js'
+pin 'jquery2', to: 'jquery.js'
+pin 'WOW', to: 'https://wowjs.uk/dist/wow.min.js'
+pin 'app', to: 'app.js'
