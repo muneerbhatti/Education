2ae447703a77c803e6a04c5226114af623424b0f
amir <amirad2020@gmail.com>
2026-01-28 14:11:24 +0500

    bundle and asset compile

---
+++ public/assets/application-df2b7f09.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "bootstrap"
+import "jquery1"
+import "jquery-appear"
+import "jquery-validator"
+import "jquery-countdown"
+import "jquery-magnific-popup"
+import "slick"
+import "jquery2"
+// import "wow"
+import "app"
+
