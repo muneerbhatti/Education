028b7eaf66edc4c518bbb1c3182e6342525bea7a
amir <amirad2020@gmail.com>
2026-02-01 19:07:11 +0500

    fix some pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "home#index"
-  get "home/about-us"
+  get "about-us", to: "home#about_us", as: :about_us
+  get "join-us", to: "home#join_us", as: :join_us
+  get "contact-us", to: "home#contact_us", as: :contact_us
+  get "team", to: "home#team", as: :team
+  get "gallery", to: "home#gallery", as: :gallery
+  get "faq", to: "home#faq", as: :faq
   get "home/courses"
   get "up" => "rails/health#show", as: :rails_health_check
 
