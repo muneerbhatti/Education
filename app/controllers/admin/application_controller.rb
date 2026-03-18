81892b4ff5afe0b55dfb659d1a176889e2c96683
amir <amirad2020@gmail.com>
2026-03-18 06:30:45 +0500

    admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,6 +7,7 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
+    before_action :authenticate_user!
 
     def authenticate_admin
       # TODO Add authentication logic here.
@@ -14,8 +15,8 @@ module Admin
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+    def records_per_page
+      params[:per_page] || 20
+    end
   end
 end
